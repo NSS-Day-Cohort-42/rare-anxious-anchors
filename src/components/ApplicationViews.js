06311d44f627b89a1022cc15e24ed44603fc7fb8
cwelshofer <cwelshofer@gmail.com>
2020-10-27 14:02:27 -0500

    working details page

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,8 +1,19 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { PostDetails } from "./posts/postDetail"
+import { PostProvider } from "./posts/postProvider"
+
+                           
 
 export const ApplicationViews = () => {
     return <>
+     <PostProvider>
+                <Route path="/posts/:postId(\d+)" render={
+                    props => <PostDetails {...props} />
+                } />
+            </PostProvider>
+            
+
         <main style={{
             margin: "5rem 2rem",
             lineHeight: "1.75rem"
---
+++ src/components/posts/postDetail.js
@@ -0,0 +1,49 @@
+import React, { useContext, useEffect, useState } from "react"
+import { PostContext } from "./postProvider"
+
+
+export const PostDetails = (props) => {
+    const { getPostById } = useContext(PostContext)
+
+    const [post, setPost] = useState({user: {}})
+
+    useEffect(() => {
+        const postId = parseInt(props.match.params.postId)
+        getPostById(postId)
+            .then(setPost)
+    }, [])
+
+    const handleDate = (date) => {
+        if("postDate" in post){
+            return new Date(date).toDateString()
+        }
+    }
+
+    return (
+        <div className="postDetailContainer">
+            <h1 className="postHeader">{post.header}</h1>
+            <h2 className="postTitle">{post.title}</h2>
+            <div className="body">
+                <h3>{post.postBody}</h3>
+                <h4>{handleDate(post.postDate)}</h4>
+                <div>{post.Author}</div>
+
+            </div>
+            <div>
+            </div>
+            <button onClick={() => {
+                props.history.push(`/tags`)
+            }}>Manage Tags</button>
+            <button onClick={() => {
+                props.history.push(`/posts`)
+            }}>Add post</button>
+            <button onClick={() => {
+                props.history.push(`/comments`)
+            }}>Show Comments</button>
+            <button onClick={() => {
+                props.history.push(`/comments`)
+            }}>Add Comment</button>
+
+        </div>
+    )
+}
